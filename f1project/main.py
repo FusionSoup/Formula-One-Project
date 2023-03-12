eb3322033747c068c10eef8623b65f7720945752
FusionSoup <acallan0@icloud.com>
2023-03-12 12:21:13 +0000

    rearranged the entire program removing globals and using ttk

--- f1project/main.py
+++ f1project/main.py
@@ -1,168 +1,148 @@
 import json
 import wikipediaapi
 from tkinter import *
+from tkinter import ttk
 from ergast import Ergast
 
-global database
-dsw = None
-csw = None
-d_frame = None
-dsw_frame = None
-csw_frame = None
 
+class F1Gui:
+    def __init__(self, root, database):
+        self.root = root
+        self.database = database
+        self.frame = ttk.Frame(self.root, padding="5")
+        self.frame.grid(column=0, row=0, sticky=[N, E, W, S])
+        self.root.columnconfigure(0, weight=1)
+        self.root.rowconfigure(0, weight=1)
+        self.dsw = None
+        self.d_frame = None
+        self.dsw_frame = None
+        self.driver_standings = None
+        self.csw = None
+        self.csw_frame = None
 
-def driver_standings_selection(event):
-    global dsw, d_frame, dsw_frame, database
-    selection = event.widget.curselection()
-    if len(selection) != 1:
-        return
-    for slave in dsw_frame.grid_slaves():
-        slave.destroy()
-    driver_key = database.driver_list[selection[0]]['driverId']
-    standings_table = database.get_driver_standings(driver_key)
-    Label(dsw_frame, text='Year').grid(row=0, column=0)
-    Label(dsw_frame, text='Position').grid(row=0, column=1)
-    Label(dsw_frame, text='Points').grid(row=0, column=2)
-    Label(dsw_frame, text='Wins').grid(row=0, column=3)
-    Label(dsw_frame, text='Constructor').grid(row=0, column=4)
-    for i in range(len(standings_table['StandingsLists'])):
-        sl = standings_table['StandingsLists'][i]
-        Label(dsw_frame, text=sl['season']).grid(row=i + 1, column=0)
-        Label(dsw_frame, text=sl['DriverStandings'][0]['position']).grid(row=i + 1, column=1)
-        Label(dsw_frame, text=sl['DriverStandings'][0]['points']).grid(row=i + 1, column=2)
-        Label(dsw_frame, text=sl['DriverStandings'][0]['wins']).grid(row=i + 1, column=3)
-        Label(dsw_frame, text=sl['DriverStandings'][0]['Constructors'][0]['name']).grid(row=i + 1, column=4)
+    def driver_standings_selection(self, event):
+        selection = event.widget.curselection()
+        if len(selection) != 1:
+            return
+        for slave in self.dsw_frame.grid_slaves():
+            slave.destroy()
+        driver_key = self.database.driver_list[selection[0]]['driverId']
+        standings_table = self.database.get_driver_standings(driver_key)
+        Label(self.dsw_frame, text='Year').grid(row=0, column=0)
+        Label(self.dsw_frame, text='Position').grid(row=0, column=1)
+        Label(self.dsw_frame, text='Points').grid(row=0, column=2)
+        Label(self.dsw_frame, text='Wins').grid(row=0, column=3)
+        Label(self.dsw_frame, text='Constructor').grid(row=0, column=4)
+        for i in range(len(standings_table['StandingsLists'])):
+            sl = standings_table['StandingsLists'][i]
+            Label(self.dsw_frame, text=sl['season']).grid(row=i + 1, column=0)
+            Label(self.dsw_frame, text=sl['DriverStandings'][0]['position']).grid(row=i + 1, column=1)
+            Label(self.dsw_frame, text=sl['DriverStandings'][0]['points']).grid(row=i + 1, column=2)
+            Label(self.dsw_frame, text=sl['DriverStandings'][0]['wins']).grid(row=i + 1, column=3)
+            Label(self.dsw_frame, text=sl['DriverStandings'][0]['Constructors'][0]['name']).grid(row=i + 1, column=4)
 
-    seasons_list = database.get_seasons_for_driver(driver_key)
-    seasons_list.reverse()
-    selected_season = StringVar()
-    selected_season.set(seasons_list[0])
-    seasons_menu = OptionMenu(d_frame, selected_season, *seasons_list)
-    seasons_menu.grid(row=0, column=0)
-    #seasons_menu.bind('<<OptionMenuSelect>>', driver_standings_selection)
+        seasons_list = self.database.get_seasons_for_driver(driver_key)
+        seasons_list.reverse()
+        selected_season = StringVar()
+        seasons_menu = OptionMenu(self.d_frame, selected_season, *seasons_list)
+        seasons_menu.grid(row=0, column=0)
+        selected_season.set(seasons_list[0])
+        # seasons_menu.bind('<<OptionMenuSelect>>', driver_standings_selection)
 
+    def circuit_selection(self, event):
+        selection = event.widget.curselection()
+        if len(selection) != 1:
+            return
+        for slave in self.csw_frame.grid_slaves():
+            slave.destroy()
+        circuit = self.database.circuit_list[selection[0]]
+        circuit_key = circuit['circuitId']
+        location_text = f"{circuit['circuitName']}, {circuit['Location']['locality']}, {circuit['Location']['country']}"
+        wiki_text = self.get_wikipedia_summary(circuit['url'].split('/')[-1])
+        Label(self.csw_frame, text=location_text).grid(row=0, column=0)
+        Label(self.csw_frame, text=wiki_text, width=80, wraplength=560, ).grid(row=2, column=0)
+        #Label(dsw_frame, text='Points').grid(row=0, column=2)
+        #Label(dsw_frame, text='Wins').grid(row=0, column=3)
+        #Label(dsw_frame, text='Constructor').grid(row=0, column=4)
+        #for i in range(len(standings_table['StandingsLists'])):
+        #    sl = standings_table['StandingsLists'][i]
+        #    Label(dsw_frame, text=sl['season']).grid(row=i + 1, column=0)
+        #    Label(dsw_frame, text=sl['DriverStandings'][0]['position']).grid(row=i + 1, column=1)
+        #    Label(dsw_frame, text=sl['DriverStandings'][0]['points']).grid(row=i + 1, column=2)
+        #    Label(dsw_frame, text=sl['DriverStandings'][0]['wins']).grid(row=i + 1, column=3)
+        #    Label(dsw_frame, text=sl['DriverStandings'][0]['Constructors'][0]['name']).grid(row=i + 1, column=4)
 
-def circuit_selection(event):
-    global csw, csw_frame, database
-    selection = event.widget.curselection()
-    if len(selection) != 1:
-        return
-    for slave in csw_frame.grid_slaves():
-        slave.destroy()
-    circuit = database.circuit_list[selection[0]]
-    circuit_key = circuit['circuitId']
-    location_text = f"{circuit['circuitName']}, {circuit['Location']['locality']}, {circuit['Location']['country']}"
-    wiki_text = get_wikipedia_summary(circuit['url'].split('/')[-1])
-    Label(csw_frame, text=location_text).grid(row=0, column=0)
-    Label(csw_frame, text=wiki_text, width=80, wraplength=560, ).grid(row=2, column=0)
-    #Label(dsw_frame, text='Points').grid(row=0, column=2)
-    #Label(dsw_frame, text='Wins').grid(row=0, column=3)
-    #Label(dsw_frame, text='Constructor').grid(row=0, column=4)
-    #for i in range(len(standings_table['StandingsLists'])):
-    #    sl = standings_table['StandingsLists'][i]
-    #    Label(dsw_frame, text=sl['season']).grid(row=i + 1, column=0)
-    #    Label(dsw_frame, text=sl['DriverStandings'][0]['position']).grid(row=i + 1, column=1)
-    #    Label(dsw_frame, text=sl['DriverStandings'][0]['points']).grid(row=i + 1, column=2)
-    #    Label(dsw_frame, text=sl['DriverStandings'][0]['wins']).grid(row=i + 1, column=3)
-    #    Label(dsw_frame, text=sl['DriverStandings'][0]['Constructors'][0]['name']).grid(row=i + 1, column=4)
+    def get_wikipedia_summary(self, page):
+        wiki_wiki = wikipediaapi.Wikipedia('en')
+        wiki = wiki_wiki.page(page)
+        return wiki.summary
 
+    def driver_standings_window_destroy(self, event):
+        self.dsw = None
 
-def get_wikipedia_summary(page):
-    wiki_wiki = wikipediaapi.Wikipedia('en')
-    wiki = wiki_wiki.page(page)
-    return wiki.summary
+    def driver_standings_window(self):
+        if self.dsw:
+            self.dsw.tkraise()
+        else:
+            self.dsw = Tk()
+            self.dsw.title('Drivers')
+            self.dsw.geometry('1000x600')
+            self.dsw.bind('<Destroy>', self.driver_standings_window_destroy)
+            driver_listbox = Listbox(self.dsw, width=30)
+            driver_listbox.bind('<<ListboxSelect>>', self.driver_standings_selection)
+            my_scroll = Scrollbar(self.dsw, command=driver_listbox.yview)
+            driver_listbox.config(yscrollcommand=my_scroll.set)
+            my_scroll.pack(side=LEFT, fill=Y)
+            driver_listbox.pack(side=LEFT, fill=Y)
+            self.d_frame = ttk.Frame(self.dsw)
+            self.d_frame.pack(side='right', fill='both')
+            self.dsw_frame = ttk.Frame(self.d_frame)
+            self.dsw_frame.grid(row=0, column=2)
 
-        # ds = st['DriverStandings']
-        #print(repr(ds))
-        #for d in ds:
-       #  print(d['Driver']['givenName'])
+            for d in database.driver_list:
+                driver_listbox.insert('end', d['givenName'] + ' ' + d['familyName'])
 
+    def circuit_window_destroy(self, event):
+        self.csw = None
 
-#def driver_selection(driver):
-    #total points, race wins, podiums etc
-    #image of driver + bio
-    #twitter feed of driver account
-    #driver_standings_selection(driver, year)
-    #race_results_selection(driver, year)
+    def circuit_window(self):
+        if self.csw:
+            self.csw.tkraise()
+        else:
+            self.csw = Tk()
+            self.csw.title('circuit information')
+            self.csw.geometry('1000x600')
+            self.csw.bind('<Destroy>', self.circuit_window_destroy)
+            circuit_listbox = Listbox(self.csw, width=30)
+            circuit_listbox.bind('<<ListboxSelect>>', self.circuit_selection)
+            my_scroll2 = Scrollbar(self.csw, command=circuit_listbox.yview)
+            circuit_listbox.config(yscrollcommand=my_scroll2.set)
+            my_scroll2.pack(side=LEFT, fill=Y)
+            circuit_listbox.pack(side=LEFT, fill=Y)
+            self.csw_frame = ttk.Frame(self.csw)
+            self.csw_frame.pack(side='top', fill=NONE, expand=True)
+            for c in self.database.circuit_list:
+                circuit_listbox.insert('end', c['circuitName'] + ', ' + c['Location']['country'])
 
-
-#def race_results_selection(year, driver, circuit):
-
-
-def driver_standings_window_destroy(event):
-    global dsw
-    dsw = None
-
-
-def driver_standings_window():
-    global dsw, d_frame, dsw_frame, database
-    if dsw:
-        dsw.tkraise()
-    else:
-        dsw = Tk()
-        dsw.title('Drivers')
-        dsw.geometry('1000x600')
-        dsw.bind('<Destroy>', driver_standings_window_destroy)
-        driver_listbox = Listbox(dsw, width=30)
-        driver_listbox.bind('<<ListboxSelect>>', driver_standings_selection)
-        my_scroll = Scrollbar(dsw, command=driver_listbox.yview)
-        driver_listbox.config(yscrollcommand=my_scroll.set)
-        my_scroll.pack(side=LEFT, fill=Y)
-        driver_listbox.pack(side=LEFT, fill=Y)
-        d_frame = Frame(dsw,)
-        d_frame.pack(side='right', fill='both')
-        dsw_frame = Frame(d_frame,)
-        dsw_frame.grid(row=0, column=2)
-
-        for d in database.driver_list:
-            driver_listbox.insert('end', d['givenName'] + ' ' + d['familyName'])
-
-
-def circuit_window_destroy(event):
-    global csw
-    csw = None
-
-
-def circuit_window():
-    global csw, csw_frame, database
-    if csw:
-        csw.tkraise()
-    else:
-        csw = Tk()
-        csw.title('circuit information')
-        csw.geometry('1000x600')
-        csw.bind('<Destroy>', circuit_window_destroy)
-        circuit_listbox = Listbox(csw, width=30)
-        circuit_listbox.bind('<<ListboxSelect>>', circuit_selection)
-        my_scroll2 = Scrollbar(csw, command=circuit_listbox.yview)
-        circuit_listbox.config(yscrollcommand=my_scroll2.set)
-        my_scroll2.pack(side=LEFT, fill=Y)
-        circuit_listbox.pack(side=LEFT, fill=Y)
-        csw_frame = Frame(csw,)
-        csw_frame.pack(side='top', fill=NONE, expand=True)
-        for c in database.circuit_list:
-            circuit_listbox.insert('end', c['circuitName'] + ', ' + c['Location']['country'])
-
-
-def main_window():
-    root = Tk()
-    root.title('formula one')
-    root.geometry('1200x900')
-    my_scroll = Scrollbar(root)
-    my_scroll.pack(side=RIGHT, fill=Y)
-    button_frame = LabelFrame(root, height=400, width=800, bd='5')
-    button_frame.pack(side='top')
-    exiting = Button(button_frame, text='exit', bd='5', height=20, width=40, command=root.destroy)
-    exiting.grid(row=0, column=0, pady=2)
-    driver_standings = Button(button_frame, text='Drivers', bd='5', height=20, width=40, command=driver_standings_window)
-    driver_standings.grid(row=0, column=1, pady=2)
-    race_results = Button(button_frame, text='race results', bd='5', height=20, width=40, command=root.destroy)
-    race_results.grid(row=0, column=2, pady=2)
-    constructors = Button(button_frame, text='constructors', bd='5', height=20, width=40, command=root.destroy)
-    constructors.grid(row=1, column=1, pady=2)
-    circuits = Button(button_frame, text='circuits', bd='5', height=20, width=40, command=circuit_window)
-    circuits.grid(row=1, column=2, pady=2)
-    root.mainloop()
+    def main_window(self):
+        self.root.title('Formula One')
+        self.root.geometry('1200x900')
+        # my_scroll = ttk.Scrollbar(self.frame)
+        # my_scroll.pack(side=RIGHT, fill=Y)
+        button_frame = ttk.LabelFrame(self.frame, height=400, width=800)
+        button_frame.pack(side='top')
+        exiting = ttk.Button(button_frame, text='exit', width=40, command=root.destroy)
+        exiting.grid(row=0, column=0, pady=2)
+        self.driver_standings = ttk.Button(button_frame, text='Drivers', width=40,
+                                           command=self.driver_standings_window)
+        self.driver_standings.grid(row=0, column=1, pady=2)
+        race_results = ttk.Button(button_frame, text='race results', width=40, command=root.destroy)
+        race_results.grid(row=0, column=2, pady=2)
+        constructors = ttk.Button(button_frame, text='constructors', width=40, command=root.destroy)
+        constructors.grid(row=1, column=1, pady=2)
+        circuits = ttk.Button(button_frame, text='circuits', width=40, command=self.circuit_window)
+        circuits.grid(row=1, column=2, pady=2)
+        root.mainloop()
 
 
 def save_database(db, filename):
@@ -178,7 +158,6 @@ def get_database_from_web(db):
 
 
 if __name__ == '__main__':
-    global database
     database = Ergast()
     try:
         # raise json.decoder.JSONDecodeError('blah', 'dgya', 1)
@@ -186,4 +165,7 @@ if __name__ == '__main__':
         save_database(database, 'database.bin')
     except (json.decoder.JSONDecodeError, ConnectionRefusedError):
         load_database(database, 'database.bin')
-    main_window()
+
+    root = Tk()
+    f1Gui = F1Gui(root, database)
+    f1Gui.main_window()
